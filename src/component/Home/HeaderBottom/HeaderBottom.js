0a8390f39cb094b6a9cbfe7e97f9fd81e797058b
muhammadtawhid <muhammadtawhid.me@gmail.com>
2022-03-18 17:29:01 +0600

    complete digital dudes

--- src/component/Home/HeaderBottom/HeaderBottom.js
+++ src/component/Home/HeaderBottom/HeaderBottom.js
@@ -14,14 +14,14 @@ const HeaderBottom = () => {
                         <h1 className="text-center">40</h1>
                     </div>
                     <div className="col-lg-6 col-md-12 col-sm-12 bal">
-                        <div className="main-content">
-                            <h4> Offices in different countries</h4>
-                            <p>When interacting with customers- we strive to make sure that we present an experience ensuring their satisfaction. Our goal is to keep the customers happy and regularly coming back.</p>
+                        <div className="main-content ">
+                            <h4>Offices in different countries</h4>
+                            <p className="main-content-p">When interacting with customers- we strive to make sure that we present an experience ensuring their satisfaction. Our goal is to keep the customers happy and regularly coming back.</p>
                             <div className="row d-flex align-items-center">
                                 <div className="col-md-4">
                                     <img src={teamWork} alt="" />
                                 </div>
-                                <div className="col-md-8 text-center">
+                                <div className="col-md-8 mt-3">
                                     <p> <FontAwesomeIcon className="icon" icon={faCheck} /> Delivering top-quality services.</p>
                                     <p> <FontAwesomeIcon className="icon" icon={faCheck} /> 24 hours project help support.</p>
                                     <p> <FontAwesomeIcon className="icon" icon={faCheck} /> Easy & trouble free handy deal</p>
