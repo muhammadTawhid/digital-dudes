70dbc337feec3eca67b62f5784d36501325ccbde
muhammadtawhid <muhammadtawhid.me@gmail.com>
2022-03-21 16:42:25 +0600

    complete digital dudes

--- src/component/Home/AppointmentAndFaq/AppointmentAndFaq.js
+++ src/component/Home/AppointmentAndFaq/AppointmentAndFaq.js
@@ -8,10 +8,10 @@ const AppointmentAndFaq = () => {
         <Fade bottom>
             <div id="aboutUs" className="container my-5 py-5">
                 <div className="row">
-                    <div className="col-md-6">
+                    <div className="col-sm-12 col-lg-6 col-md-12 ">
                         <Appointment />
                     </div>
-                    <div className="col-md-6">
+                    <div className="col-sm-12 col-lg-6 col-md-12">
                         <Faq />
                     </div>
                 </div>
