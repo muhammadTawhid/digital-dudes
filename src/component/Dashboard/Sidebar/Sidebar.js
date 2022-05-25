cf2902312458543cd0dd1b8dfd368d68e6561573
muhammadtawhid <muhammadtawhid.me@gmail.com>
2022-05-26 00:21:51 +0600

    complete digital dudes

--- src/component/Dashboard/Sidebar/Sidebar.js
+++ src/component/Dashboard/Sidebar/Sidebar.js
@@ -1,24 +1,47 @@
 import './Sidebar.css';
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../../images/logo.png';
 import { SidebarAdminData } from '../SidebarAdminData/SidebarAdminData';
 import { SidebarUserData } from '../SidebarUserData/SidebarUserData';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowRight, faTimes, faBars, faArrowLeft, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
+import { faTimes, faBars, faArrowLeft, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 import { UserContext } from '../../../App';
+import useWindowDimensions from '../../Hooks/useWindowDimensions';
 
 const Sidebar = () => {
+  const { width } = useWindowDimensions();
   const [loggedInUser, setLoggedInUser] = useContext(UserContext);
   const [sidebar, setSidebar] = useState(true);
 
-  const showSidebar = () => setSidebar(!sidebar);
+    useEffect(() => {
+    document.title = "Dashboard"
+  }, []);
+
+
+  const showSidebar = () => {
+    console.log("!sidebar")
+    setSidebar(!sidebar)
+  };
+
+  console.log(sidebar, "beforo ");
+  console.log(width);
+
+  const sidebarDisable = () => {
+    console.log("into the fun")
+    if (width <= 575) {
+      setSidebar(false);
+    }
+    else {
+      setSidebar(true);
+    }
+  }
 
   return (
     <>
       <div className="sidebar">
         <div className={sidebar ? "menu clicked" : "menu"} onClick={showSidebar}>
-          <FontAwesomeIcon icon={faBars} onClick={showSidebar} />
+          <FontAwesomeIcon icon={faBars} />
         </div>
         <div>
           <img
@@ -27,9 +50,6 @@ const Sidebar = () => {
             alt=""
           />
         </div>
-        {/* <Link to="#" className="menu-bars">
-          <FontAwesomeIcon icon={faArrowRight} onClick={showSidebar} />
-        </Link> */}
       </div>
       <div className={sidebar ? "sidebar-menu sidebar-active" : "sidebar-menu"}>
         <ul className="sidebar-menu-items">
@@ -54,18 +74,18 @@ const Sidebar = () => {
             loggedInUser.admin ?
               SidebarAdminData.map((item, index) => {
                 return (
-                  <li key={index} className={item.cName}>
-                    <Link to={item.path}>
-                      {item.icon}
-                      <span className="sidebar-title">{item.title}</span>
-                    </Link>
-                  </li>
+                    <li key={index} className={item.cName} onClick={sidebarDisable}>
+                      <Link to={item.path} >
+                        {item.icon}
+                        <span className="sidebar-title">{item.title}</span>
+                      </Link>
+                    </li>
                 )
               })
               :
               SidebarUserData.map((item, index) => {
                 return (
-                  <li key={index} className={item.cName}>
+                  <li key={index} className={item.cName} onClick={sidebarDisable}>
                     <Link to={item.path}>
                       {item.icon}
                       <span className="sidebar-title">{item.title}</span>
@@ -87,88 +107,4 @@ const Sidebar = () => {
 };
 
 export default Sidebar;
-
-
-
-
-// import './Sidebar.css';
-// import React, { useState, useEffect, useContext } from 'react';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faStar, faArrowRight, faArrowLeft, faSignOutAlt, faTasks, faFolderPlus, faMoneyCheckAlt, faFileInvoiceDollar, faUserPlus, faHouseUser } from '@fortawesome/free-solid-svg-icons';
-// import logo from '../../../images/logo.png';
-// import { Link } from 'react-router-dom';
-// import { UserContext } from '../../../App';
-
-
-// const Sidebar = () => {
-//   const [sidebar, setSidebar] = useState(true);
-//   const [loggedInUser, setLoggedInUser] = useContext(UserContext)
-//   console.log(sidebar, "sidebar");
-//   useEffect(() => {
-//     document.title = "Dashboard"
-//   }, []);
-
-//   const handleLogOut = () => {
-//     setLoggedInUser("")
-//   }
-//   return (
-
-//       <div className={sidebar ? "sidebar-on" : " sidebar-off"}>
-//         <div className="sidebar-header">
-//           <Link to="/">
-//             <img src={logo} alt="" />
-//           </Link>
-//           <button onClick={() => setSidebar(!sidebar)}>{sidebar ? <FontAwesomeIcon icon={faArrowLeft} /> : <FontAwesomeIcon icon={faArrowRight} />}</button>
-//         </div>
-
-//         <div className="sidebar-content">
-//           {loggedInUser.admin && <div>
-//             <div className="">
-//               <Link to="/dashboard/manageService">
-//                 <button className="d-flex justify-content-around"><span><FontAwesomeIcon icon={faTasks} /></span> <span>Manage Service</span></button>
-//               </Link>
-//             </div>
-//             <div className="">
-//               <Link to="/dashboard/service/addService">
-//                 <button className="d-flex justify-content-around"><span><FontAwesomeIcon icon={faFolderPlus} /></span> <span>Add Service</span></button>
-//               </Link>
-//             </div>
-//             <div className="">
-//               <Link to="/dashboard/editPricing">
-//                 <button className="d-flex justify-content-around"><span><FontAwesomeIcon icon={faFileInvoiceDollar} /></span><span>Edit Pricing</span></button>
-//               </Link>
-//             </div>
-//             <div className="">
-//               <Link to="/dashboard/addAdmin">
-//                 <button className="d-flex justify-content-around"><span><FontAwesomeIcon icon={faUserPlus} /></span><span>Add Admin</span></button>
-//               </Link>
-//             </div>
-//           </div>}
-//           <div>
-//             <Link to="/dashboard/subscription">
-//               <button className="d-flex justify-content-around"><span><FontAwesomeIcon icon={faMoneyCheckAlt} /></span> <span>{loggedInUser.admin ? "Subscripted User" : "Your Subscription"}</span></button>
-//             </Link>
-//           </div>
-//           <div>
-//             <Link to="/dashboard/addReview">
-//               <button className="d-flex justify-content-around"><span><FontAwesomeIcon icon={faStar} /></span> <span>Add Review</span></button>
-//             </Link>
-//           </div>
-//           <div>
-//             <Link to="/">
-//               <button className="d-flex justify-content-around"><span><FontAwesomeIcon icon={faHouseUser} /></span> <span>Back to home</span></button>
-//             </Link>
-//           </div>
-
-//         </div>
-
-//         <div className="sidebar-footer">
-//           <button id={loggedInUser.admin ? "" : "nonAdminButton"} onClick={handleLogOut}><FontAwesomeIcon icon={faSignOutAlt} /> Log Out</button>
-//         </div>
-//       </div>
-
-//   );
-// };
-
-// export default Sidebar;
 
